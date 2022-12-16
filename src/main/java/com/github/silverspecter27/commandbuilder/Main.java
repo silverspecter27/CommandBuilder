9f30a6aeabecbd4b7571c54b3b26fa2e310aaa3e
henri <henrique002176@gmail.com>
2022-12-16 12:16:45 -0300

    message

---
+++ src/main/java/com/github/silverspecter27/commandbuilder/Main.java
@@ -0,0 +1,13 @@
+package com.github.silverspecter27.commandbuilder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
